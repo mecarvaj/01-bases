ceb8937aeb566c29ea79a9f8386d15d2de7a15f2
y <mike.carvajal.carvajal@intel.com>
2022-02-10 10:09:19 -0600

    Cap 4.43-44

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -5,5 +5,23 @@ import { Component } from "@angular/core";
     templateUrl: 'heroe.component.html'
 })
 export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad  : number = 45;
+
+    
+    public getNombreCapitalizado() : string {
+        return this.nombre.toUpperCase(); 
+    }
+    
+    obtenerNombre(): string{
+        return `${this.nombre } - ${this.edad}`;
+    }
+    cambiarNombre(): void{
+        this.nombre = 'Spiderman'
+    }
+    
+    public setEdad() {
+        this.edad= 30;
+    }
     
 }
